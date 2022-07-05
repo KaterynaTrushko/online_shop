4b242fc80f37e7102a04dc0007b9779600e5339f
KaterynaTrushko <trushkokateryna@gmail.com>
2022-07-05 09:13:55 +0100

    deploy

--- src/page/Review/Review.tsx
+++ src/page/Review/Review.tsx
@@ -1,26 +1,60 @@
 import React from "react";
+import { data } from "./data";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, EffectFade, Pagination } from "swiper";
 import "swiper/scss";
-import { EffectFade } from "swiper";
 import "swiper/scss/effect-fade";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/scss/autoplay";
+import style from "./Review.module.scss";
 
 export default function Review() {
   return (
-    // <Swiper modules={[EffectFade]} effect="fade">
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      {[1, 2, 3, 5].map((i, el) => {
-        return (
-          <SwiperSlide>
-            Slide{el}
-            <img src="image/blog-1.jpg" />
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+    <>
+      <div className={style.review}>
+        <h1 className={style.title}>
+          client's <span>review</span>
+        </h1>
+        <Swiper
+          modules={[Pagination, Autoplay, Navigation]}
+          effect="fade"
+          spaceBetween={50}
+          slidesPerView={3}
+          breakpoints={{
+            250: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            900: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          {data.map((el, i) => {
+            return (
+              <SwiperSlide key={i}>
+                <div className={style.slide}>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Consequuntur veniam deserunt praesentium natus quibusdam ea
+                    nam commodi.
+                  </p>
+                  <div className={style.user}>
+                    <img src={el.img} alt="photo" />
+                    <div className={style.info}>
+                      <h3>john deo</h3>
+                      <span>happy client</span>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </>
   );
 }
---
+++ src/page/Review/data.tsx
@@ -0,0 +1,7 @@
+export const data = [
+  { img: "image/pic-1.png" },
+  { img: "image/pic-2.png" },
+  { img: "image/pic-3.png" },
+  { img: "image/pic-4.png" },
+  { img: "image/pic-5.png" },
+];
